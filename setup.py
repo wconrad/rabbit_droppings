164506af812d899eb23ceca4e1c139c5d7838f84
Wayne Conrad <wayne@databill.com>
2015-07-21 09:20:31 -0700

    Tweak setup

--- setup.py
+++ setup.py
@@ -12,17 +12,19 @@ setup(
     author='Wayne Conrad',
     author_email='kf7qga@gmail.com',
     description='Backup/Restore RabbitMQ queues',
-    long_description_markdown_filename='README.markdown',
-    name=NAME,
-    version=VERSION,
-    packages=['lib/rabbit_droppings'],
-    setup_requires=['setuptools-markdown'],
-    scripts=SCRIPTS,
     install_requires=[
         # Pika does not use semantic versioning.  Breaking changes may
         # be introduced with a bump in patch level, which is why we
         # ask for an exact patch level.
         "pika==0.9.14",
     ],
+    keywords = "rabbit rabbitmq backup restore",
+    long_description_markdown_filename='README.markdown',
+    name=NAME,
+    packages=['lib/rabbit_droppings'],
+    scripts=SCRIPTS,
+    setup_requires=['setuptools-markdown'],
     test_suite="tests",
+    url="https://github.com/wconrad/rabbit_droppings",
+    version=VERSION,
 )
